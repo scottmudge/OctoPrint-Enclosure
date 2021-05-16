3d0e1b809ae280bba193fddc41dfe90109956aa1
Scott Mudge <mail@scottmudge.com>
2021-05-15 22:10:22 -0400

    fixed support HTU21D

--- octoprint_enclosure/SI7021.py
+++ octoprint_enclosure/SI7021.py
@@ -1,49 +1,21 @@
-import smbus
 import time
-import sys
+import board
+import busio
+from adafruit_htu21d import HTU21D
+
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = HTU21D(i2c)
 
 
-if len(sys.argv) == 2 or len(sys.argv) == 3:
-    address = int(sys.argv[1],16)
-    if len(sys.argv) == 3:
-        busNum = int(sys.argv[2],16)
-    else:
-        busNum = 1
-else:
-    print('-1 | -1')
-    sys.exit(1)
+def main():
+    try:
+        # Convert the data
+        humidity = sensor.relative_humidity
+        # Convert the data
+        cTemp = sensor.temperature
+        print('{0:0.1f} | {1:0.1f}'.format(cTemp, humidity))
+    except:
+        print('-1 | -1')
 
-# Get I2C bus
-bus = smbus.SMBus(busNum)
-
-# SI7021 address, 0x40(64)
-#		0xF5(245)	Select Relative Humidity NO HOLD master mode
-bus.write_byte(address, 0xF5)
-
-time.sleep(0.3)
-
-# SI7021 address, 0x40(64)
-# Read data back, 2 bytes, Humidity MSB first
-data0 = bus.read_byte(address)
-data1 = bus.read_byte(address)
-
-# Convert the data
-humidity = ((data0 * 256 + data1) * 125 / 65536.0) - 6
-
-time.sleep(0.3)
-
-# SI7021 address, 0x40(64)
-#		0xF3(243)	Select temperature NO HOLD master mode
-bus.write_byte(address, 0xF3)
-
-time.sleep(0.3)
-
-# SI7021 address, 0x40(64)
-# Read data back, 2 bytes, Temperature MSB first
-data0 = bus.read_byte(address)
-data1 = bus.read_byte(address)
-
-# Convert the data
-cTemp = ((data0 * 256 + data1) * 175.72 / 65536.0) - 46.85
-
-print('{0:0.1f} | {1:0.1f}'.format(cTemp, humidity))
+if __name__ == "__main__":
+    main()
